7725512c6b70dde902ab651dcec577576f47dd23
leejaebae <leejaewon0227@gmail.com>
2023-08-11 19:35:47 +0900

    Initial Commit

---
+++ first/firstApp.swift
@@ -0,0 +1,17 @@
+//
+//  firstApp.swift
+//  first
+//
+//  Created by 이재원 on 2023/08/11.
+//
+
+import SwiftUI
+
+@main
+struct firstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
